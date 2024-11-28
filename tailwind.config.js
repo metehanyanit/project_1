e2f5ce041fd69628440979c58c8cb61d0a98750f
metehanyanit <metehanyanit12@gmail.com>
2024-11-28 15:39:01 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        parchment: {
+          DEFAULT: '#F4E4BC',
+          dark: '#E6D5BC',
+        }
+      },
+      fontFamily: {
+        medieval: ['MedievalSharp', 'serif'],
+        manuscript: ['Alegreya', 'serif'],
+      },
+    },
   },
   plugins: [],
 };
